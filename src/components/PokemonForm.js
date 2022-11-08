e57aeede46aa5eecedf5935269330c9a641c73f0
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-11-07 19:43:00 -0500

    finished deliverables up until POST request. Need to resolve next.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,29 +1,73 @@
-import React from "react";
+import React, {useState} from "react";
 import { Form } from "semantic-ui-react";
 
 function PokemonForm() {
+
+
+
+  const [formData, setFormData] = useState({
+    name: "",
+    hp: "",
+    frontUrl: "",
+    backUrl: ""
+  })
+
+  const handleForm = ( e )=> {
+    e.preventDefault();
+    
+    fetch("http://localhost:3000/pokemon", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData)
+    })
+  }
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    let value = e.target.value;
+    
+
+    setFormData ( {...formData, [name]: value});
+  }
+
+
+
   return (
     <div>
       <h3>Add a Pokemon!</h3>
       <Form
-        onSubmit={() => {
-          console.log("submitting form...");
-        }}
+        onSubmit={handleForm}
       >
         <Form.Group widths="equal">
-          <Form.Input fluid label="Name" placeholder="Name" name="name" />
-          <Form.Input fluid label="hp" placeholder="hp" name="hp" />
+          <Form.Input 
+            fluid label="Name" 
+            placeholder="Name" 
+            name="name"
+            value={formData.name}
+            onChange={handleChange} />
+          <Form.Input 
+            fluid label="hp" 
+            placeholder="hp" 
+            name="hp"
+            value={formData.hp}
+            onChange={handleChange} />
           <Form.Input
             fluid
             label="Front Image URL"
             placeholder="url"
             name="frontUrl"
+            value={formData.frontUrl}
+            onChange={handleChange}
           />
           <Form.Input
             fluid
             label="Back Image URL"
             placeholder="url"
             name="backUrl"
+            value={formData.backUrl}
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Button>Submit</Form.Button>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Search() {
+function Search({searchFunction}) {
+
+  // const [searchResult, setSearchResult] = useState("")
+
+
+  // const handleSearchResults = (e)=> {
+  //   setSearchResult(e.target.value);
+  // }
+  
+  function searchValue (e){
+    searchFunction(e.target.value);
+  }
+
+
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input onChange={searchValue} className="prompt" />
         <i className="search icon" />
       </div>
     </div>
