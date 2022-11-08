e700abfc1e6987e86fdd04358bf08cf14a0afcdf
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-11-07 19:42:30 -0500

    commit tree 1ac299694e1a5ead48421065325ef64a24a39561

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({id, name, hp, sprites}) {
+
+  // {
+  //   "id": 2,
+  //   "name": "ivysaur",
+  //   "hp": 60,
+  //   "sprites": {
+  //     "front": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+  //     "back": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/2.png"
+  //   }
+  // },
+
+  const [pokemonImage, setPokemonImage] = useState(true);
+
+  const handlePokemonImage = ()=> {
+    setPokemonImage(!pokemonImage);
+  }
+
+  const imageFlip = pokemonImage ? <img alt="front" src={sprites.front} /> : <img alt="back" src={sprites.back} />
+  
+
+
+
   return (
     <Card>
-      <div>
+      <div onClick={handlePokemonImage}>
         <div className="image">
-          <img alt="oh no!" />
+          {/* <img alt="oh no!" /> */}
+          {imageFlip}
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp}
           </span>
         </div>
       </div>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+
+  const [pokemonData, setPokemonData] = useState([])
+  const [searchResults, setSearchResult] = useState("")
+
+
+
+  useEffect(()=> {
+    fetch("http://localhost:3000/pokemon")
+    .then(response => response.json())
+    .then(data => setPokemonData(data))
+  }, [])
+
+  function handleSearchResult (result){
+    setSearchResult(result);
+  }
+
+  const filteredPokemon = pokemonData.filter((pokemon)=> {
+    return (pokemon.name.includes(searchResults.toLowerCase()))
+  })
+
+
+
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
       <PokemonForm />
       <br />
-      <Search />
+      <Search searchFunction={handleSearchResult}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemonData={filteredPokemon}/>
     </Container>
   );
 }
